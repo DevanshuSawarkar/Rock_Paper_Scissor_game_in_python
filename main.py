32683d830da40fb4036d20a038c3369c4e41cb39
DevanshuSawarkar <118257949+DevanshuSawarkar@users.noreply.github.com>
2023-09-04 15:15:30 +0530

    Comment Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-import random
-list1 = ["Rock", "Paper", "Scissor"]
-choice = input("Enter Rock, Paper or Scissor: ")
+# importing random to use randint() function
+import random 
+
+# Create a list that the computer can choose from.
+list1 = ["Rock", "Paper", "Scissor"] 
+
+# taking users' input
+choice = input("Enter Rock, Paper or Scissor: ") 
+
+ # Generating a random element from the list
 choice2 = list1[random.randint(0,2)]
+
+# Displaying the random choice of computer
 print("Computer choice = ", choice2)
-if choice==list1[0]:
-  if choice2==list1[0]:
+
+if choice==list1[0]:  # checking if users input is Rock
+  if choice2==list1[0]:  # checking if computer choice is also Rock
     print("Draw")
-  elif choice2==list1[1]:
+  elif choice2==list1[1]:  # checking if computer choice is Paper
     print("You Loose")
-  else:
+  else:  # user won because computer chose Scissors
     print("You Won")
-elif choice==list1[1]:
-  if choice2==list1[0]:
+elif choice==list1[1]:  # checking if users input is Paper
+  if choice2==list1[0]:  # checking if computer choice is Rock
     print("You Won")
-  elif choice2==list1[1]:
+  elif choice2==list1[1]:  # checking if computer choice is also Paper
     print("Draw")
-  else:
+  else: # user lost because computer chose Scissors
     print("You Loose")
-elif choice==list1[2]:
-  if choice2==list1[0]:
+elif choice==list1[2]:  # checking if users input is Scissors
+  if choice2==list1[0]:  # checking if computer choice is Rock
     print("You Loose")
-  elif choice2==list1[1]:
+  elif choice2==list1[1]:  # checking if computer choice is Paper
     print("You Won")
-  else:
+  else:  # its a draw because computer also chose Scissors
     print("You Draw")
 else:
-  print("Wrong input")
+  print("Wrong input")  # in case of user enters undesired input
